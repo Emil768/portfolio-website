de478a3b9185445aa7eccdd9cf539a3660f6470f
EmilMurahas228 <murakhas@list.ru>
2022-09-10 22:14:17 +0300

    fix: transfer projects array

--- src/pages/Projects/index.jsx
+++ src/pages/Projects/index.jsx
@@ -1,56 +1,11 @@
 import React from "react";
 
 import styles from "./Projects.module.scss";
-
-import {
-  comixImage,
-  trelloImage,
-  telegrambotImage,
-  notesImage,
-  vkloaderIMage,
-} from "../../img/index";
 import ProjectBlock from "../../components/ProjectBlock";
 
+import { projects } from "../../projects";
+
 function Projects() {
-  const projects = [
-    {
-      title: "React-notes",
-      img: notesImage,
-      tags: [
-        "ReactJs  NodeJs  React-redux Axios MongoDB Express Cloudinary JWTB crypt",
-      ],
-      link: "https://mern-notes-frontend.vercel.app/",
-      gitLink: "https://github.com/EmilMurahas228/mern-notes-frontend",
-    },
-    {
-      title: "VK-loader",
-      img: vkloaderIMage,
-      tags: ["ReactJs NodeJs Express Multer"],
-      link: "https://emilmurahas228.github.io/vk-loader/",
-      gitLink: "https://github.com/EmilMurahas228/vk-loader",
-    },
-    {
-      title: "Trello",
-      img: trelloImage,
-      tags: ["HTML CSS Javascript"],
-      link: " https://emilmurahas228.github.io/Trello-clone-js/",
-      gitLink: "https://github.com/EmilMurahas228/Trello-clone-js",
-    },
-    {
-      title: "React-comix",
-      img: comixImage,
-      tags: ["ReactJs json-server"],
-      link: "https://react-comix.herokuapp.com/",
-      gitLink: "https://github.com/EmilMurahas228/React-comix",
-    },
-    {
-      title: "Telegram-translate-bot",
-      img: telegrambotImage,
-      tags: ["ReactJs NodeJs Telegraf"],
-      link: "https://t.me/qirimTatat_bot",
-      gitLink: "https://github.com/EmilMurahas228/telegram-translate-bot",
-    },
-  ];
   return (
     <section className={styles.projects}>
       <h1 className={styles.projects__mainTitle}>My Works</h1>
---
+++ src/projects.js
@@ -0,0 +1,46 @@
+import {
+  comixImage,
+  trelloImage,
+  telegrambotImage,
+  notesImage,
+  vkloaderIMage,
+} from "./img/index";
+export const projects = [
+  {
+    title: "React-notes",
+    img: notesImage,
+    tags: [
+      "ReactJs  NodeJs  React-redux Axios MongoDB Express Cloudinary JWTB crypt",
+    ],
+    link: "https://mern-notes-frontend.vercel.app/",
+    gitLink: "https://github.com/EmilMurahas228/mern-notes-frontend",
+  },
+  {
+    title: "VK-loader",
+    img: vkloaderIMage,
+    tags: ["ReactJs NodeJs Express Multer"],
+    link: "https://emilmurahas228.github.io/vk-loader/",
+    gitLink: "https://github.com/EmilMurahas228/vk-loader",
+  },
+  {
+    title: "Trello",
+    img: trelloImage,
+    tags: ["HTML CSS Javascript"],
+    link: " https://emilmurahas228.github.io/Trello-clone-js/",
+    gitLink: "https://github.com/EmilMurahas228/Trello-clone-js",
+  },
+  {
+    title: "React-comix",
+    img: comixImage,
+    tags: ["ReactJs json-server"],
+    link: "https://react-comix.herokuapp.com/",
+    gitLink: "https://github.com/EmilMurahas228/React-comix",
+  },
+  {
+    title: "Telegram-translate-bot",
+    img: telegrambotImage,
+    tags: ["ReactJs NodeJs Telegraf"],
+    link: "https://t.me/qirimTatat_bot",
+    gitLink: "https://github.com/EmilMurahas228/telegram-translate-bot",
+  },
+];
